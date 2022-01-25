c656f948e6378623c337b53660ce9cede520ae9a
Endre Krohn <endre-krohn@outlook.com>
2022-01-25 14:22:38 +0000

    Add: Restore script

--- restore.sh
+++ restore.sh
@@ -1,4 +1,37 @@
-echo "Starting backup prompt"
-echo "What is the backup you want to use? [ex: filename.gz]"
-read filename
-(cd backups && gunzip < $filename | docker-compose exec -i db psql -U postgres) # -d your-db-name
+BACKUP=$(cd backups && ls -tp | grep -v /$ | head -1)
+
+if [ -z "$BACKUP" ]
+then
+  echo "No backup found!"
+  exit 1
+else
+  echo "Backup: $BACKUP"
+fi
+
+BACKUP_PATH="./backups/$BACKUP"
+
+
+CONTAINER=$(docker ps --format "{{.Names}}" | grep postgres)
+echo "Container: $CONTAINER"
+
+echo "Copy backup to container"
+(docker cp $BACKUP_PATH $CONTAINER:/tmp)
+
+
+
+exec () {
+    (docker exec -d $CONTAINER  \
+    $1)
+}
+
+echo "Remove all active sessions"
+(docker exec -d $CONTAINER psql -U postgres -d db -c "SELECT pid, pg_terminate_backend(pid), datname FROM pg_stat_activity WHERE datname = current_database() AND pid <> pg_backend_pid();")
+
+echo "Drop database and create new"
+exec "dropdb -U postgres --if-exists db"
+exec "createdb -U postgres db"
+
+echo "Loading database backup"
+(docker exec -d $CONTAINER sh -c "gunzip -c /tmp/$BACKUP | psql -U postgres db")
+exec "rm /tmp/$BACKUP"
+
